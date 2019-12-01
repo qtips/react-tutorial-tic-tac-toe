72a3bc93cc989bedb266cf926e0852b120e5ebde
qtips <ahm.qadeer@gmail.com>
2019-12-01 21:21:24 +0100

    ferdig fungerende spill utenom historikk

--- my-app-v1/src/index.js
+++ my-app-v1/src/index.js
@@ -18,47 +18,68 @@ import './index.css';
 
  immutability i react brukes til å enkelt avgjøre når et objekt endrer seg. For å vite om et Mutable objekt har endret seg krever at man må sjekke felt for felt.   
  */
-class Square extends React.Component {
-    /*
-    Ingen state på square, da den styres fullstendig av Board
-    */
-    render() {
-        return (
-            <button className="square"
-                    onClick={this.props.vedKlikk}>
-                {this.props.value /* hver gang state til board endrer seg så oppdateres props */}
-            </button>
-        );
-    }
+
+
+/** function componenent - enklere måte å lage klasser som kun består av render
+ * Se under
+ **/
+function Square(props) {
+    return (
+        <button className="square"
+                onClick={props.vedKlikk}>
+            {props.value /* hver gang state til board endrer seg så oppdateres props */}
+        </button>
+    );
+
 }
+
+/*** klassen under er skrevet om til Function Component over! ***/
+// class Square extends React.Component {
+//     /*
+//     Ingen state på square, da den styres fullstendig av Board
+//     */
+//     render() {
+//         return (
+//             <button className="square"
+//                     onClick={this.props.vedKlikk}>
+//                 {this.props.value /* hver gang state til board endrer seg så oppdateres props */}
+//             </button>
+//         );
+//     }
+// }
 
 class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
-            lastMove: null
+            nextMove: 'X'
         }
     }
 
     renderSquare(i) {
         return (
-            <Square value={this.state.squares[i] /*binder state med props*/ }
+            <Square value={this.state.squares[i] /*binder state med props*/}
                     vedKlikk={() => this.nextSquare(i)}
             />
         );
     }
 
     nextSquare(i) {
-        let nextMove = this.state.lastMove && this.state.lastMove === 'X'
-            ? 'O' : 'X';
-        let squares = this.state.squares.slice(); // kopiere array
-        squares[i] = nextMove;
-        this.setState({squares, lastMove: nextMove});
+        if (calculateWinner(this.state.squares)) {
+            return;
+        }
+        const squares = this.state.squares.slice(); // kopiere array
+        squares[i] = this.state.nextMove;
+        const nextMove = this.state.nextMove === 'X' ? 'O' : 'X';
+        this.setState({squares, nextMove});
     }
 
     render() {
-        const status = 'Next player: X';
+        const winner = calculateWinner(this.state.squares);
+        const status = winner ?
+            winner + " has won!"
+            : 'Next player: ' + this.state.nextMove;
 
         return (
             <div>
@@ -105,3 +126,23 @@ ReactDOM.render(
     <Game/>,
     document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
